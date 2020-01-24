dbc73bf0a51fa6eef9a940e08de602baf1b4ae50
moffire <alexeydudarev.spb@gmail.com>
2020-01-24 14:49:59 +0300

    Added user model and registration/auth via Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'pages#landing'
+
+  #header
   get 'about' => 'pages#about'
+
+  #registration/authentication
+  devise_scope :user do
+    get 'authenticate' => 'devise/sessions#new'
+    get 'registration' => 'devise/registrations#new'
+    delete 'sign_out' => 'devise/sessions#destroy'
+  end
+
 end
