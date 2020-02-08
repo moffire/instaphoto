6231516534f92cfe215a72295fae0f2b93914898
moffire <alexeydudarev.spb@gmail.com>
2020-02-08 15:45:48 +0300

    added validates for User and Photo models

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,7 @@ class Photo < ApplicationRecord
   belongs_to :user
   has_attached_file :image, styles: { medium: "356x356>", thumb: "100x100>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  validates :description, presence: true
+  validates :image, presence: truex
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, presence: true
+
   has_many :photos, dependent: :destroy
   has_many :subscriptions, dependent: :destroy
 end
